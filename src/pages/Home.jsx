3b23581d64e6bf914b3a14b6dd84265ecf0bb1f2
Adem-Tozlu <Adem-Tozlu@hotmail.de>
2024-06-13 17:09:43 +0200

    Die Daten wurden erfolgreich aus der dummy API gezogen und zu Homepage hinzugefügt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,25 @@ import {  useContext } from 'react'
 
 function Home() {
     const {products} = useContext(FetchAPIContext)
+    
     return (
       <>
-        {products.map((product) => (
-          <div key={product.id}><h3>{product.id}</h3>
+        {products && products.map((product) => {
+            const { depth, height, width } = product.dimensions || {};
+     return (
+          <div key={product.id}>
+            {product.images.map((image,index) =>  {
+                return <img key={index} src={image} alt={image.alt} />
+            })}
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <p>Depth: {depth}</p>
+              <p>Height: {height}</p>
+              <p>Width: {width}</p>
           </div>
-        ))}
+            )
+})}
       </>
     );
 }
